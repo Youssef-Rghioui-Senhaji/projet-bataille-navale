c2cfbbdf7d4fca4e7fedef52855896c4532c21c4
Youssef-Rghioui-Senhaji <youssefrghioui590@gmail.com>
2025-09-26 22:10:00 +0200

    Update fichier test_grille

--- test_grille.py
+++ test_grille.py
@@ -8,3 +8,15 @@ def test_init():
     assert g.colonnes == 8
     assert len(g.matrice) == 5
     assert len(g.matrice[0]) == 8
+
+
+
+
+def test_afficher(capsys):
+    g = Grille(2, 2)
+    g.tirer(0, 0)
+    g.afficher()
+    captured = capsys.readouterr()
+    assert "X" in captured.out
+    assert "." in captured.out
+
