6730c8969680c8e36c780ccb06d53bcad2ced98f
Youssef-Rghioui-Senhaji <youssefrghioui590@gmail.com>
2025-09-26 23:26:06 +0200

    Update story_bateau.py

--- story_bateau.py
+++ story_bateau.py
@@ -2,14 +2,13 @@ from bateau import Bateau
 
 
 def chevauchent(b1: Bateau, b2: Bateau) -> bool:
-    """Retourne True si les deux bateaux se chevauchent"""
     return any(pos in b2.positions for pos in b1.positions)
 
 
 def main():
     b1 = Bateau(2, 3, longueur=3)  
 
-    b2 = Bateau(2, 5, longueur=2)  
+    b2 = Bateau(6, 8, longueur=2)  
 
     if chevauchent(b1, b2):
         print("Les bateaux se chevauchent !")
