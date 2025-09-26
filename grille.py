618b0527e7b2c94b7e6124360b38da9ee356cc37
Youssef-Rghioui-Senhaji <youssefrghioui590@gmail.com>
2025-09-26 22:02:41 +0200

    Update grille.py

--- grille.py
+++ grille.py
@@ -3,3 +3,10 @@ class Grille:
         self.lignes = lignes
         self.colonnes = colonnes
         self.matrice = [[0 for _ in range(colonnes)] for _ in range(lignes)]
+
+    def afficher(self):
+        for row in self.matrice:
+            print(" ".join("X" if cell else "." for cell in row))
+
+    def tirer(self, x, y):
+        self.matrice[y][x] = 1
