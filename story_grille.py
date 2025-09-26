d2fc71057d53b18bf682dbde66c1789fb15e4af1
Youssef-Rghioui-Senhaji <youssefrghioui590@gmail.com>
2025-09-26 21:04:53 +0200

    :bookmark:Création de la user story_grille

---
+++ story_grille.py
@@ -0,0 +1,20 @@
+# from grille import Grille
+
+# grille = Grille(5, 8)
+
+#    while True:
+
+#        grille.afficher()
+
+#        try:
+#            x = int(input("Entrez la colonne (x) : "))
+#            y = int(input("Entrez la ligne (y) : "))
+#        except ValueError:
+#            print("Veuillez entrer des nombres valides.")
+#            continue
+
+#        if not (0 <= x < grille.colonnes) or not (0 <= y < grille.lignes):
+#            print("Coordonnées hors de la grille, réessayez.")
+#            continue
+
+#        grille.tirer(x, y)
